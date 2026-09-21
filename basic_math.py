7678cef259026a497aa935a597ead25440e620dd
Joyceline Pangning <joyceline.pangning-mfadja@stu.ocsb.ca>
2026-09-21 12:48:34 +0000

    Completed basic math operators assignment with required math functions

--- basic_math.py
+++ basic_math.py
@@ -9,13 +9,13 @@ def main():
     print("4*6={}".format(4 * 6))
     print("2+5*3={}".format(2 + 5 * 3))
     print("2^3={}".format(2 ^ 3))  # bitwise operator
-    print("2**3={}".format(2**3))  # exponent
+    print("2**3={}".format(2 ** 3))  # exponent
     print("8%3={}".format(8 % 3))  # modulo
-    print("3**2={}".format(3**2))  # powers
-    print("9**0.5={}".format(9**0.5))  # square root
+    print("3**2={}".format(3 ** 2))  # powers
+    print("9**0.5={}".format(9 ** 0.5))  # square root
     print("9**(1/3)={}".format(9 ** (1 / 3)))  # cube root
     print("9/4={}".format(9 / 4))
-    print("√25={}".format(25**0.5))
+    print("√25={}".format(25 ** 0.5))
 
 if __name__ == "__main__":
     main()
