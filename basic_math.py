e29182669cb8c323f9fb715751f033581824b7c3
Joyceline Pangning <joyceline.pangning-mfadja@stu.ocsb.ca>
2026-09-22 13:34:47 +0000

    added spaces

--- basic_math.py
+++ basic_math.py
@@ -3,13 +3,14 @@
 # Date: September 19th, 2026
 # This program performs basic math operations
 
+
 def main():
     print("5-1={}".format(5 - 1))
     print("5+3={}".format(5 + 3))
     print("4*6={}".format(4 * 6))
     print("2+5*3={}".format(2 + 5 * 3))
     print("2^3={}".format(2 ^ 3))  # bitwise operator
-    print("2**3={}".format(2 ** 3))  # exponent
+    print("2**3={}".format(2**3))  # exponent
     print("8%3={}".format(8 % 3))  # modulo
     print("3**2={}".format(3 ** 2))  # powers
     print("9**0.5={}".format(9 ** 0.5))  # square root
@@ -17,6 +18,6 @@ def main():
     print("9/4={}".format(9 / 4))
     print("√25={}".format(25 ** 0.5))
 
-    
-    if __name__ == "__main__":
+
+if __name__ == "__main__":
     main()
